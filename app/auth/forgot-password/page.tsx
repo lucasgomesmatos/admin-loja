104870743fb78ddaa088942dad982e57e7efdf7a
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-03-31 18:09:56 -0300

    feat: suspense

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/card";
 import { Library } from "lucide-react";
 import { Metadata } from "next";
+import { Suspense } from "react";
 import { FormForgotPassword } from "../components/form-forgot-password";
 
 export const metadata: Metadata = {
@@ -26,7 +27,9 @@ export default async function ForgotPassword() {
           </CardDescription>
         </CardHeader>
         <CardContent className="w-96">
-          <FormForgotPassword />
+          <Suspense>
+            <FormForgotPassword />
+          </Suspense>
         </CardContent>
       </Card>
     </main>
